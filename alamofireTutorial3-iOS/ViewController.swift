2e064db7253666434058737e0d07a37cad2abb79
hyungyu Kim <ypsophila97@icloud.com>
2021-03-15 22:12:34 +0900

    Add Response Handling remark

--- alamofireTutorial3-iOS/ViewController.swift
+++ alamofireTutorial3-iOS/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
     func getTest() {
         print("onClickedPost()")
         let url = "https://jsonplaceholder.typicode.com/todos/1"
-        AF.request(url).responseJSON{
+        AF.request(url).validate().responseJSON{
             response in
             switch response.result {
             case .success(let value):
@@ -42,21 +42,42 @@ class ViewController: UIViewController {
                 let userId = json["userId"].intValue
                 let id = json["id"].intValue
                 let title = json["title"].stringValue
-                
+
                 self.userIdLabel.text = String(userId)
                 self.idLabel.text = String(id)
                 self.titleLabel.text = title
-                
+
             case .failure(let err):
                 print(err)
             }
+//        AF.request(url).validate().responseString{
+//            response in
+//            switch response.result {
+//            case .success(let value):
+//                print("GET success!")
+//                print(value)
+//            case .failure(let err):
+//                print(err)
+//            }
+//        AF.request(url).validate().responseData{
+//                    response in
+//                    switch response.result {
+//                    case .success(let value):
+//                        print("GET success!")
+//                        print(value)
+//                    case .failure(let err):
+//                        print(err)
+//                    }
         }
+        //Response Validation
+        //기본적으로 알라모파이어는 응답내용에 관계없이 완료된 요청을 성공으로 처리한다. validate() 를 지정하면 허용되지 않는 상태코드에 대해서 오류가 생성된다.(200..<300) 보통 200이 성공. 우리가 잘 아는 404.\
+        
         //GET request 의 response Handling
-        //response
-        //responseJSON
-        //responseString
-        //responseData
-        //URLEncodedFormEncoder : 
+        //response : response data 에 대해서 검증하지 않는다. 바로 URLSessionDelegate 로 보냄.
+        //responseData : 서버에서 반환된 Data 를 추출하고 유효성 검사.
+        //responseJSON : 서버에서 반환되 Data 를 Any 타입의 json object 로 변환
+        //responseString : 서버에서 반환된 Data 를 문자열로 변환
+        //URLEncodedFormEncoder : 서버에서 반환된 Data 를 전달된 Decodable 타입으로 변환
         
     }
     func postTest() {
@@ -104,6 +125,9 @@ class ViewController: UIViewController {
         //-POST 에서는 기본적으로 http body 로 설정해서 json 으로 인코딩 된 값이 전달.
         //{"id":"1234","pwd":"1234"} 형태로 전달
         //header 로 authorize
+        //해야함
+        
+
         
     }
 }
